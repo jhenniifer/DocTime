065f01213ac7af33bd4fc48c42be915f76dac3ed
jhenniifer <chukwumakosisochukwu7@gmail.com>
2024-07-07 18:29:04 +0100

    home/faq mobile added

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function navigateToDoctors(event) {
     event.preventDefault();
     window.location.href = '/html/doctors.html';
 }
+function backBtn(event){
+    event.preventDefault();
+    window.history.back();
+}
+function displayMenu(event){
+    event.preventDefault();
+    document.getElementById('aside').style.display ='block';
+}
